c3b1c787f81aea48243bdc2217acab6b3123c0f2
mhenriquelima <mhenriquedelima1@gmail.com>
2025-03-01 23:41:51 -0300

    ✨ : banco de dados externo

--- project/settings.py
+++ project/settings.py
@@ -75,10 +75,16 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crudDjango',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
+}
 }
 
 
